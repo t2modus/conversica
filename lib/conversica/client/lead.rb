ddbb42c2ec930fac9581d442db29ff1be6e8d702
Andy Kallhoff <andykallhoff@gmail.com>
2020-03-05 11:39:28 -0700

    modified errors, should be easier for dream side

--- lib/conversica/client/lead.rb
+++ lib/conversica/client/lead.rb
@@ -8,23 +8,17 @@ module Conversica
 
       PERMITTED_COLUMNS = %w[
         address alt_email api_version appointment_date appointment_status bdc_rep_id bdc_rep_email bdc_rep_name
-        cell_phone city client_id conversation_id date email first_name home_email home_phone id last_name lead_source
-        lead_status lead_type make model opt_out rep_id rep_email rep_name service_rep_id service_rep_email
-        service_rep_name sms_opt_out state skip_to_followup stop_messaging vin work_email work_phone year zip
+        cell_phone city client_id conversation_id date email first_name home_email home_phone id last_name
+        lead_source lead_status lead_type make model opt_out rep_id rep_email rep_name service_rep_id
+        service_rep_email service_rep_name sms_opt_out state skip_to_followup stop_messaging vin work_email
+        work_phone year zip
       ].freeze
-
-      BOOLEAN_COLUMNS = Set.new(
-        %w[stop_messaging skip_to_followup opt_out sms_opt_out]
-      ).freeze
-
-      DATE_COLUMNS = Set.new(
-        %w[date appointment_date]
-      ).freeze
-
       REQUIRED_ATTRIBUTES = %w[
-        api_version bdc_rep_email bdc_rep_name conversation_id email first_name id lead_source lead_status make model
-        rep_email rep_name year
+        api_version bdc_rep_email bdc_rep_name conversation_id email first_name id lead_source lead_status
+        year make model rep_email rep_name
       ].freeze
+      BOOLEAN_COLUMNS = %w[stop_messaging skip_to_followup opt_out sms_opt_out].freeze
+      DATE_COLUMNS = %w[date appointment_date].freeze
 
       define_attr_accessors_for_attributes
 
@@ -38,12 +32,13 @@ module Conversica
       end
 
       def valid?
-        self.validate_type
+        self.validate_types
+        self.validate_dates
         self.validate_required
         self.errors.count.zero?
       end
 
-      def validate_type
+      def validate_types
         PERMITTED_COLUMNS.each do |k|
           permitted_classes, error = if BOOLEAN_COLUMNS.include?(k)
                                        [
@@ -52,23 +47,32 @@ module Conversica
                                        ]
                                      elsif DATE_COLUMNS.include?(k)
                                        [
-                                         [NilClass, Date, Time],
-                                         "#{k} must be null or a valid date or time object"
+                                         [NilClass, Date, Time, String],
+                                         "#{k} must be null or a valid date, time, or parsable string object"
                                        ]
                                      else
                                        [
-                                         [NilClass, String],
-                                         "#{k} must be null or a string"
+                                         [NilClass, Integer, String],
+                                         "#{k} must be null, integer, or a string"
                                        ]
                                      end
           # We actually really truly do need the case equality operator here to determine if
           # the attribute is an instance of a permitted class
           # To do this, we must make sure that the class is FIRST, since
-          # Class === instance_of_class is true, but instance_of_class === Class
-          # is not
+          # Class === instance_of_class is true, but instance_of_class === Class is not
           # rubocop:disable Style/CaseEquality
           self.errors << error unless permitted_classes.any? { |c| c === self.attributes[k] }
           # rubocop:enable Style/CaseEquality
+        end
+      end
+
+      def validate_dates
+        dates = self.attributes.slice(*DATE_COLUMNS).compact
+        return if dates.empty?
+        dates.each do |key, val|
+          self.attributes[key] = val.to_datetime.rfc3339
+        rescue ArgumentError
+          self.errors << "#{key} is not a valid parsable date string, value passed: #{val.inspect}"
         end
       end
 
@@ -82,11 +86,11 @@ module Conversica
       def conversicate
         hash = self.attributes.dup
 
-        DATE_COLUMNS.each_with_object(hash) do |k, h|
-          value = h[k]
-          next if value.nil?
-          h[k] = value.to_datetime.rfc3339
-        end
+        # Apparently for conversica:
+        # 1) they do not like to receive nil values so we need to remove the nils
+        # 2) we also need to convert integers into strings
+        hash.reject! { |_k, v| v.nil? }
+        hash.transform_values! { |v| v = v.to_s if v.is_a?(Integer) }
 
         hash.transform_keys { |k| k.camelize(:lower) }
       end
--- lib/conversica/version.rb
+++ lib/conversica/version.rb
@@ -2,5 +2,5 @@
 
 # This module is the base level entry point for all things conversica
 module Conversica
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 end
